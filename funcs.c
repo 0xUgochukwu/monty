d6fe89c4ae52946428907f541650bd584725a5cb
0xUgochukwu <0xUgochukwu@gmail.com>
2023-05-24 11:09:02 -0700

    pop

--- funcs.c
+++ funcs.c
@@ -5,7 +5,7 @@
  * @stack: Double pointer to the head of the stack.
  * @line_number: current line number
  */
-void push(stack_t **stack, unsigned int line_number);
+void push(stack_t **stack, unsigned int line_number)
 {
 	stack_t *new_node = malloc(sizeof(stack_t));
 
