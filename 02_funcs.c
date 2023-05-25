636dc9195a67c77d6471b80233c508f4f0db2d6b
0xUgochukwu <0xUgochukwu@gmail.com>
2023-05-25 04:55:35 -0700

    div

--- 02_funcs.c
+++ 02_funcs.c
@@ -38,12 +38,36 @@ void nop(stack_t **stack, unsigned int line_number)
  */
 void sub(stack_t **stack, unsigned int line_number)
 {
-    if (*stack == NULL || (*stack)->next == NULL)
-    {
-        fprintf(stderr, "L%d: can't sub, stack too short\n", line_number);
-        exit(EXIT_FAILURE);
-    }
+	if (*stack == NULL || (*stack)->next == NULL)
+	{
+		fprintf(stderr, "L%d: can't sub, stack too short\n", line_number);
+		exit(EXIT_FAILURE);
+	}
 
-    (*stack)->next->n -= (*stack)->n;
-    pop(stack, line_number);
+	(*stack)->next->n -= (*stack)->n;
+	pop(stack, line_number);
 }
+
+/**
+ * div - Divides the second top element by the top element of the stack.
+ * @stack: Double pointer to the head of the stack.
+ * @line_number: Current line number.
+ */
+void div(stack_t **stack, unsigned int line_number)
+{
+	if (*stack == NULL || (*stack)->next == NULL)
+	{
+		fprintf(stderr, "L%d: can't div, stack too short\n", line_number);
+		exit(EXIT_FAILURE);
+	}
+
+	if ((*stack)->n == 0)
+	{
+		fprintf(stderr, "L%d: division by zero\n", line_number);
+		exit(EXIT_FAILURE);
+	}
+
+	(*stack)->next->n /= (*stack)->n;
+	pop(stack, line_number);
+}
+
