dbe16dce8c3dd68768df0d9255df3ca75ea453fb
0xUgochukwu <0xUgochukwu@gmail.com>
2023-05-25 05:35:51 -0700

    stack fix

--- 01_funcs.c
+++ 01_funcs.c
@@ -7,7 +7,7 @@
  */
 void push(stack_t **stack, unsigned int line_number)
 {
-	stack_t *new_node = malloc(sizeof(stack_t));
+	stack_t *new_node;
 
 	if (!argument || _isdigit(argument) == 1)
 	{
@@ -16,6 +16,7 @@ void push(stack_t **stack, unsigned int line_number)
 		exit(EXIT_FAILURE);
 	}
 
+	new_node = malloc(sizeof(stack_t));
 	if (new_node == NULL)
 	{
 		fprintf(stderr, "Error: malloc failed\n");
